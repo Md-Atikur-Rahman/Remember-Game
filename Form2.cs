29cd2c131c27ab7281d1af64c73b88a0f6f5b691
MD-Atikur-Rahman <atik9991234@gmail.com>
2022-03-26 17:49:36 +0600

    level 2 added

--- Form2.cs
+++ Form2.cs
@@ -7,72 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Threading;
 
 namespace Memory_Game
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        Thread th;
-
         Random random = new Random();
 
         List<string> icons = new List<string>();
         List<string> big = new List<string>();
 
         Label first, second;
-        int cnt=0;
-        public Form1()
+        int cnt = 0;
+        public Form2()
         {
             InitializeComponent();
-            assigning();  
-        }
-
-        private void label_click(object sender, EventArgs e)
-        {
-            if (first != null && second != null)
-                return;
-
-            Label clickedlbl = sender as Label;
-            if(clickedlbl == null)
-                return;
-
-            if (clickedlbl.ForeColor == Color.Black)
-                return;
-
-            if(first == null)
-            {
-                first = clickedlbl;
-                first.ForeColor = Color.Black;
-                return;
-            }
-
-            second = clickedlbl;
-            second.ForeColor = Color.Black;
-
-            if(first.Text == second.Text)
-            {
-                first = null;
-                second = null;
-                cnt+=2;
-
-                if(cnt==16)
-                {
-                    MessageBox.Show("Congratulation. You matched all. Move to level 2");
-                    this.Close();
-                    th = new Thread(opennewform);
-                    th.SetApartmentState(ApartmentState.STA);
-                    th.Start();
-                }
-            }
-            else
-                timer1.Start(); 
-
-        }
-
-        private void opennewform()
-        {
-            Application.Run(new Form2());
+            assigning();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -86,6 +36,44 @@ namespace Memory_Game
             second = null;
         }
 
+        private void btn_click(object sender, EventArgs e)
+        {
+            if (first != null && second != null)
+                return;
+
+            Label clickedlbl = sender as Label;
+            if (clickedlbl == null)
+                return;
+
+            if (clickedlbl.ForeColor == Color.Black)
+                return;
+
+            if (first == null)
+            {
+                first = clickedlbl;
+                first.ForeColor = Color.Black;
+                return;
+            }
+
+            second = clickedlbl;
+            second.ForeColor = Color.Black;
+
+            if (first.Text == second.Text)
+            {
+                first = null;
+                second = null;
+                cnt += 2;
+
+                if (cnt == 36)
+                {
+                    MessageBox.Show("Congratulation. You complete level 2.");
+                    this.Close();
+                }
+            }
+            else
+                timer1.Start();
+        }
+
         private void assigning()
         {
 
@@ -94,21 +82,21 @@ namespace Memory_Game
             Label label;
             int rndnom;
 
-            for(int i=33; i<127; i++)
+            for (int i = 33; i < 127; i++)
             {
                 if (i == 38) continue;
                 s = "";
                 ch = Convert.ToChar(i);
-                s = s+ch.ToString();
+                s = s + ch.ToString();
 
                 big.Add(s);
             }
 
             Console.WriteLine(big.Count);
 
-            for(int i=0; i<8; i++)
+            for (int i = 0; i < 18; i++)
             {
-                rndnom=random.Next(0,big.Count);
+                rndnom = random.Next(0, big.Count);
                 icons.Add(big[rndnom]);
                 icons.Add(big[rndnom]);
 
@@ -116,13 +104,13 @@ namespace Memory_Game
             }
 
 
-            for(int i = 0; i < tableLayoutPanel1.Controls.Count; i++)
+            for (int i = 0; i < tableLayoutPanel1.Controls.Count; i++)
             {
                 if (tableLayoutPanel1.Controls[i] is Label)
                     label = (Label)tableLayoutPanel1.Controls[i];
                 else continue;
 
-                rndnom = random.Next(0,icons.Count);
+                rndnom = random.Next(0, icons.Count);
                 label.Text = icons[rndnom];
 
                 icons.RemoveAt(rndnom);
